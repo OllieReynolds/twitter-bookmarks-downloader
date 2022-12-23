36265da3bc49e8a593c98045a8e85401895c923e
OllieReynolds <cubed@outlook.com>
2022-12-23 11:24:02 +0100

    Update to use oauthv2 flow and v2 twitter api

--- main.py
+++ main.py
@@ -1,85 +1,114 @@
+import base64
+import hashlib
 import os
-import requests
+import re
 import json
+import requests
+from requests.auth import HTTPBasicAuth
+from requests_oauthlib import OAuth2Session
+import configparser
 
-# Replace these with your own Twitter API keys and secrets
-consumer_key = "YOUR_CONSUMER_KEY"
-consumer_secret = "YOUR_CONSUMER_SECRET"
-access_token = "YOUR_ACCESS_TOKEN"
-access_token_secret = "YOUR_ACCESS_TOKEN_SECRET"
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
-# Set up the authentication
-auth = requests.auth.OAuth1(
-    consumer_key, consumer_secret, access_token, access_token_secret
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+client_id = config["DEFAULT"]["client_id"]
+client_secret = config["DEFAULT"]["client_secret"]
+redirect_uri = config["DEFAULT"]["redirect_uri"]
+
+# Set the scopes
+scopes = ["bookmark.read", "tweet.read", "users.read", "offline.access"]
+
+# Create a code verifier
+code_verifier = base64.urlsafe_b64encode(os.urandom(30)).decode("utf-8")
+code_verifier = re.sub("[^a-zA-Z0-9]+", "", code_verifier)
+
+# Create a code challenge
+code_challenge = hashlib.sha256(code_verifier.encode("utf-8")).digest()
+code_challenge = base64.urlsafe_b64encode(code_challenge).decode("utf-8")
+code_challenge = code_challenge.replace("=", "")
+
+# Start an OAuth 2.0 session
+oauth = OAuth2Session(client_id, redirect_uri=redirect_uri, scope=scopes)
+
+# Create an authorize URL
+auth_url = "https://twitter.com/i/oauth2/authorize"
+authorization_url, state = oauth.authorization_url(
+    auth_url, code_challenge=code_challenge, code_challenge_method="S256"
 )
 
-# Set up the base URL for the Twitter API
-base_url = "https://api.twitter.com/1.1/bookmarks/list.json"
+# Visit the URL to authorize your App to make requests on behalf of a user
+print(
+    "Visit the following URL to authorize your App on behalf of your Twitter handle in a browser:"
+)
+print(authorization_url)
 
-# Set up the parameters for the API request
-params = {
-    "count": 100,  # The number of bookmarks to retrieve (up to a maximum of 100)
-    "full_text": "true",  # Include the full text of the tweet in the response
+# Paste in your authorize URL to complete the request
+authorization_response = input(
+    "Paste in the full URL after you've authorized your App:\n"
+)
+
+# Fetch your access token
+token_url = "https://api.twitter.com/2/oauth2/token"
+
+auth = HTTPBasicAuth(client_id, client_secret)
+
+token = oauth.fetch_token(
+    token_url=token_url,
+    authorization_response=authorization_response,
+    auth=auth,
+    client_id=client_id,
+    include_client_id=True,
+    code_verifier=code_verifier,
+)
+
+# Your access token
+access = token["access_token"]
+
+# Make a request to the users/me endpoint to get your user ID
+user_me = requests.request(
+    "GET",
+    "https://api.twitter.com/2/users/me",
+    headers={"Authorization": "Bearer {}".format(access)},
+).json()
+user_id = user_me["data"]["id"]
+
+# Make a request to the bookmarks url
+url = "https://api.twitter.com/2/users/{}/bookmarks".format(user_id)
+headers = {
+    "Authorization": "Bearer {}".format(access),
+    "User-Agent": "BookmarksSampleCode",
 }
 
-# Create the downloads directory if it doesn't exist
-if not os.path.exists("downloads"):
-    os.mkdir("downloads")
+# Initialize variables to store the bookmarks and pagination token
+bookmarks = []
+pagination_token = None
 
-# Keep making API requests until we have retrieved all bookmarks
 while True:
-    # Make the API request
-    response = requests.get(base_url, auth=auth, params=params)
+    # Set the pagination token in the query parameters if it exists
+    params = {}
+    if pagination_token:
+        params["pagination_token"] = pagination_token
 
-    # Check the status code of the response
-    if response.status_code == 200:
-        # If the request was successful, parse the JSON data
-        data = json.loads(response.text)
-        bookmarks = data["bookmarks"]  # This is a list of bookmark objects
+    # Make the request
+    response = requests.request("GET", url, headers=headers, params=params)
+    if response.status_code != 200:
+        raise Exception(
+            "Request returned an error: {} {}".format(response.status_code, response.text)
+        )
+    print("Response code: {}".format(response.status_code))
+    json_response = response.json()
 
-        # Iterate through the list of bookmarks
-        for bookmark in bookmarks:
-            tweet = bookmark["status"]  # This is a tweet object
+    # Append the bookmarks from the response to the list of bookmarks
+    bookmarks += json_response["data"]
 
-            # Create a directory for this bookmark
-            bookmark_dir = f"downloads/tweet_{tweet['id']}"
-            os.mkdir(bookmark_dir)
+    # Get the pagination token from the response
+    pagination_token = json_response.get("pagination_token")
 
-            # Save the text of the tweet to a file
-            with open(f"{bookmark_dir}/tweet.txt", "w") as f:
-                f.write(tweet["text"])
-
-            # Save any attached media to files
-            media = tweet["entities"].get("media", [])
-            for idx, m in enumerate(media):
-                # Determine the file extension based on the media type
-                media_type = m["type"]
-                if media_type == "photo":
-                    file_ext = "jpg"
-                elif media_type == "video":
-                    file_ext = "mp4"
-                elif media_type == "animated_gif":
-                    file_ext = "gif"
-                else:
-                    file_ext = "unknown"
-
-
-                # Download the media and save it to a file
-                media_url = m["media_url_https"]
-                r = requests.get(media_url)
-                with open(f"{bookmark_dir}/media_{idx}.{file_ext}", "wb") as f:
-                    f.write(r.content)
-
-        # Check
-        if "next_cursor" in data and data["next_cursor"] != "0":
-            # If there are more bookmarks, update the max_id parameter and make another request
-            params["max_id"] = (
-                data["bookmarks"][-1]["status"]["id"] - 1
-            )  # Subtract 1 to avoid duplicates
-        else:
-            # If there are no more bookmarks, break out of the loop
-            break
-    else:
-        # If the request was not successful, print the error message and break out of the loop
-        print(f"Error: {response.text}")
+    # If the pagination token is None, it means we have reached the last page of bookmarks
+    if pagination_token is None:
         break
+
+print(bookmarks)
+
